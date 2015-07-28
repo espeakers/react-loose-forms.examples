ed7634242530d846ecae5f38d20a89d32831bbdf
Matthew Wright <mwright@espeakers.com>
2015-07-28 13:49:06 -0600

    done with basic login form example

--- login/LoginForm.js
+++ login/LoginForm.js
@@ -6,24 +6,47 @@ module.exports = dd.createClass({
   buildSchema: function(){
     return {
       username: {
-        type: "text"
+        type: "text",
+        placeholder: "Username"
       },
       password: {
-        type: "password"
+        type: "password",
+        placeholder: "Password",
+        validate: function(v){
+          if(!v || v.length === 0){
+            return "Please enter your password";
+          }
+          if(v.length < 8){
+            return "Passwords must be at least 8 characters long";
+          }
+          return true;
+        }
       }
     };
   },
   render: function(){
-    var fields = this.buildSchema();
+    var fields = this.Form_buildSchema();
     var errors = this.state.errors || {};
 
     return dd.form({onSubmit: this.Form_onSubmit},
 
-      Form_buildInput(fields, "username"),
-      Form_buildInput(fields, "password"),
+      dd.div(null,
+        this.Form_buildInput(fields["username"]),
 
-      dd.button({type: "submit"}, "log in")
+        errors["username"] ? dd.b(null, "Error: ", errors["username"]) : null
+      ),
 
+
+      dd.div(null,
+        this.Form_buildInput(fields["password"]),
+
+        errors["password"] ? dd.b(null, "Error: ", errors["password"]) : null
+      ),
+
+
+      dd.div(null,
+        dd.button({type: "submit"}, "log in")
+      )
     );
   }
 });
--- login/index.js
+++ login/index.js
@@ -1,17 +1,22 @@
 var dd = require("react-dd");
 var React = require("react");
 require("../bootstrap.min.css");
-require("react-loose-forms.bootstrap3").install();
+require("react-loose-forms.bootstrap3").install(require("react-loose-forms/InputTypes"));
 
 var LoginForm = require("./LoginForm");
 
 var App = dd.createClass({
+  __handleLoginSubmit: function(data){
+
+    console.log("TODO login", data.username, data.password);
+
+  },
   render: function(){
     return dd.div({style: {margin: "50px", width: 500}},
       dd.div({className: "well"},
         dd.h3({style: {marginTop: 0}}, "ACME login"),
 
-        LoginForm()
+        LoginForm({onSubmit: this.__handleLoginSubmit})
       )
     );
   }
